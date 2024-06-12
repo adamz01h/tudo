55b329abb58b050ef5a14eb7ac88f9eb16173481
adamz01h <adamz01h@gmail.com>
2024-06-12 14:08:13 -0400

    sure

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -2,14 +2,12 @@
     if (!isset($db)) {
        $dbhost        = "127.0.0.1";
        $dbport        = "3306";
-       $dbname      = "tudo";
+       $dbname        = "tudo";
        $dbusername    = "tudo";
        $dbpassword    = "tudo1@1!";
 
-       $db = new mysqli("$dbhost", "$dbusername", "$dbpassword", "$dbname");
-0
-       if (!$db) {
+       $db = new mysqli($dbhost, $dbusername, $dbpassword, $dbname);
+       if(!$db) {
            echo "Error: Unable to connect to db.";
        }
     }
-?>

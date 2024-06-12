4f79514dbcbc6ee24a9b15f69c3a1203f358838a
adamz01h <adamz01h@gmail.com>
2024-06-12 17:42:09 -0400

    update

--- index.php
+++ index.php
@@ -17,19 +17,29 @@
             <div id="index_content">
                 <?php if (isset($_SESSION['isadmin'])) {
                     include('includes/db_connect.php');
-                    $ret = pg_query($db, "select * from users order by uid asc;");
+                    // Execute the query to select all users ordered by uid in ascending order
+                    $result = $db->query("SELECT * FROM users ORDER BY uid ASC");
 
+                    // Check for errors
+                    if ($db->error) {
+                        die("Error executing query: " . $db->error);
+                    }
+
+                    // Display the results
                     echo '<h4>[Admin Section]</h4>';
                     echo '<table>';
                     echo '<tr><th>Uid</th><th>Username</th><th>Password (SHA256)</th><th>Description</th></tr>';
-                    while ($row = pg_fetch_row($ret)) {
+
+                    // Fetch and display each row
+                    while ($row = $result->fetch_row()) {
                         echo '<tr>';
-                        echo '<td>'.$row[0].'</td>';
-                        echo '<td>'.$row[1].'</td>';
-                        echo '<td>'.$row[2].'</td>';
-                        echo '<td>'.$row[3].'</td>';
+                        echo '<td>' . htmlspecialchars($row[0], ENT_QUOTES, 'UTF-8') . '</td>';
+                        echo '<td>' . htmlspecialchars($row[1], ENT_QUOTES, 'UTF-8') . '</td>';
+                        echo '<td>' . htmlspecialchars($row[2], ENT_QUOTES, 'UTF-8') . '</td>';
+                        echo '<td>' . htmlspecialchars($row[3], ENT_QUOTES, 'UTF-8') . '</td>';
                         echo '</tr>';
                     }
+
                     echo '</table><br>';
                     echo '<b>Import user:</b> <br>';
                 ?>
@@ -50,8 +60,16 @@
                         echo '</a>';
 
                     include('includes/db_connect.php');
-                    $ret = pg_query($db, "select * from motd;");
-                    $row = pg_fetch_row($ret);
+                    // Execute the query to select all records from the motd table
+                    $result = $db->query("SELECT * FROM motd");
+
+                    // Check for errors
+                    if ($db->error) {
+                        die("Error executing query: " . $db->error);
+                    }
+
+                    // Fetch the first row
+                    $row = $result->fetch_row();
                     require_once 'vendor/autoload.php';
                     $smarty = new Smarty();
                     $smarty->assign("username", $_SESSION['username']);
@@ -59,29 +77,47 @@
                     $smarty->force_compile = true;
                     echo $smarty->fetch("motd.tpl").'<br>';
 
-                    $ret = pg_query($db, "select * from motd_images order by iid desc limit 3;");
-                    while($row = pg_fetch_row($ret)) {
-                        echo '<figure><img src="'.$row[1].'" /><figcaption>'.$row[2].'</figcaption></figure>';
+                    // Execute the query to select the last 3 records from the motd_images table ordered by iid in descending order
+                    $result = $db->query("SELECT * FROM motd_images ORDER BY iid DESC LIMIT 3");
+
+                    // Check for errors
+                    if ($db->error) {
+                        die("Error executing query: " . $db->error);
                     }
+
+                    // Fetch and display each row
+                    while ($row = $result->fetch_row()) {
+                        echo '<figure><img src="' . htmlspecialchars($row[1], ENT_QUOTES, 'UTF-8') . '" /><figcaption>' . htmlspecialchars($row[2], ENT_QUOTES, 'UTF-8') . '</figcaption></figure>';
+                    }
+
+
                     echo '</div>';
                     echo '<hr>';
                 ?>
 
                 <?php
                     include('includes/db_connect.php');
-                    $ret = pg_query($db, "select * from class_posts;");
-
+               
                     echo '<h4>[All Posts]</h4>';
                     echo '<table id="class_posts">';
                     echo '<tr><th>Lva Code</th><th>Lva Name</th><th>Professor</th>';
                     echo '<th>ECTS</th><th>Comment</th></tr>';
-                    while ($row = pg_fetch_row($ret)) {
+                    // Execute the query to select all records from the class_posts table
+                    $result = $db->query("SELECT * FROM class_posts");
+
+                    // Check for errors
+                    if ($db->error) {
+                        die("Error executing query: " . $db->error);
+                    }
+
+                    // Fetch and display each row
+                    while ($row = $result->fetch_row()) {
                         echo '<tr>';
-                        echo '<td><i>'.htmlentities($row[1]).'</i></td>';
-                        echo '<td><u>'.htmlentities($row[2]).'</u></td>';
-                        echo '<td>'.htmlentities($row[3]).'</td>';
-                        echo '<td>'.htmlentities($row[4]).'</td>';
-                        echo '<td>'.htmlentities($row[5]).'</td>';
+                        echo '<td><i>' . htmlspecialchars($row[1], ENT_QUOTES, 'UTF-8') . '</i></td>';
+                        echo '<td><u>' . htmlspecialchars($row[2], ENT_QUOTES, 'UTF-8') . '</u></td>';
+                        echo '<td>' . htmlspecialchars($row[3], ENT_QUOTES, 'UTF-8') . '</td>';
+                        echo '<td>' . htmlspecialchars($row[4], ENT_QUOTES, 'UTF-8') . '</td>';
+                        echo '<td>' . htmlspecialchars($row[5], ENT_QUOTES, 'UTF-8') . '</td>';
                         echo '</tr>';
                     }
                     echo '</table><hr>';

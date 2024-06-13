009b9aa84b9f4e9f15217fbb8b74c106a0cdd598
adamz01h <adamz01h@gmail.com>
2024-06-12 21:23:35 -0400

    updated header

--- includes/header.php
+++ includes/header.php
@@ -1,13 +1,13 @@
-<?php 
-    if (session_id() == "")
-        session_start(); 
+<?php
+if (session_id() == "")
+    session_start();
 ?>
 
 <div id="header">
-<b><a href="/">TUDO</a></b> -- <i>An anonymous forum for discussing classes at the <a href="">Technical University of Vienna</a></i>
+    <b><a href="/">TUDO</a></b> -- <i>An anonymous forum for discussing classes at the <a href="">Technical University of Vienna</a></i>
 
-<?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true){?>
-    <span style="float:right;">Logged in as: <a href="/profile.php"><b><?php echo $_SESSION['username']; ?></b></a>, <a href="/includes/logout.php">Log out</a></span>
-<?php } ?>
-<small style="color:#003366"><a href="">&copy; William Moody, 2021</a></small>
+    <?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) { ?>
+        <span style="float:right;">Logged in as: <a href="/profile.php"><b><?php echo $_SESSION['username']; ?></b></a>, <a href="/includes/logout.php">Log out</a></span>
+    <?php } ?>
+    <small style="color:#003366"><a href="">&copy; <?php echo date("Y"); ?> </a></small>
 </div>
